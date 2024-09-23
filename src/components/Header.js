c55692f1e795e5be3d45979621f8699995b63c56
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2024-09-24 01:16:50 +0200

    feat: add header component for HRnet

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Header.css'; // Si tu veux ajouter un fichier CSS spécifique
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>HRnet</h1>
+    </header>
+  );
+};
+
+export default Header;
